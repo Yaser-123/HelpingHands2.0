3fc31b917c246ec3aeb1d3bd67d30ac9498207fd
externref <shiva02939@gmail.com>
2024-06-19 17:10:43 +0530

    update password len

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,15 +1,15 @@
-CREATE TABLE "users"(
+CREATE TABLE IF NOT EXISTS "users"(
     "userId" VARCHAR(10) NOT NULL,
     "userName" VARCHAR(40) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "role" VARCHAR(255) NOT NULL,
-    "password" VARCHAR(10) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
     "createdAt" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     "updated_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
 );
 ALTER TABLE
     "users" ADD PRIMARY KEY("userId");
-CREATE TABLE "companies"(
+CREATE TABLE IF NOT EXISTS "companies"(
     "companyId" VARCHAR(10) NOT NULL,
     "companyName" VARCHAR(40) NOT NULL,
     "address" VARCHAR(255) NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE "companies"(
 );
 ALTER TABLE
     "companies" ADD PRIMARY KEY("companyId");
-CREATE TABLE "job_applications"(
+CREATE TABLE IF NOT EXISTS "job_applications"(
     "userId" VARCHAR(10) NOT NULL,
     "jobId" VARCHAR(10) NOT NULL,
     "createdAt" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE "job_applications"(
 );
 ALTER TABLE
     "job_applications" ADD PRIMARY KEY("userId");
-CREATE TABLE "jobs"(
+CREATE TABLE IF NOT EXISTS "jobs"(
     "jobId" VARCHAR(10) NOT NULL,
     "title" VARCHAR(40) NOT NULL,
     "jobDescription" VARCHAR(4000) NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE "jobs"(
 );
 ALTER TABLE
     "jobs" ADD PRIMARY KEY("jobId");
-CREATE TABLE "courses"(
+CREATE TABLE IF NOT EXISTS "courses"(
     "courseId" VARCHAR(10) NOT NULL,
     "courseName" VARCHAR(255) NOT NULL,
     "instituteName" VARCHAR(255) NOT NULL,
